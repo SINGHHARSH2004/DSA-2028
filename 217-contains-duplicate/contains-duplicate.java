2041058e5a64b8337472f3a9bd976f6e8aca9142
SINGHHARSH2004 <singhh14587@gmail.com>
2026-07-28 22:49:41 +0530

    Time: 13 ms (95.03%) | Memory: 109.1 MB (7.83%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -3,15 +3,16 @@ import java.util.*;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
     HashSet<Integer> set= new HashSet<>();
-    for( int items:nums){
-        set.add(items);
-    }  
-    if(set.size()==nums.length){
-        return false;
-    } 
-    else{
-      return true;
+    // for( int items:nums){
+    //     set.add(items);
+    // }
+    for (int num : nums) {
+    if (!set.add(num)) {   // add() returns false if already present
+        return true;       // duplicate found
     }
+}
+   
+  return false;
     
     }
 }
