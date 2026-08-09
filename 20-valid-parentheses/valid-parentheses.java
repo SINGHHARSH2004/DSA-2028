1452238c235d5ff2b29c7795afc792efab2a23ea
SINGHHARSH2004 <singhh14587@gmail.com>
2026-08-09 20:12:35 +0530

    Time: 3 ms (86.00%) | Memory: 43.6 MB (15.42%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,29 +2,39 @@
 
 class Solution {
     public boolean isValid(String s) {
-    Stack<Character> stack = new Stack<>();
 
-    for (char ch : s.toCharArray()) {
-      if (ch == '(' || ch == '{' || ch == '[') {
-        stack.push(ch);
-      } else {
-        if (ch == ')') {
-          if (stack.isEmpty() || stack.pop() != '(') {
-            return false;
-          }
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            }
+
+            else if (ch == ')') {
+                if (stack.isEmpty() || stack.peek() != '(') {
+                    return false;
+                }
+                stack.pop();
+            }
+
+            else if (ch == ']') {
+                if (stack.isEmpty() || stack.peek() != '[') {
+                    return false;
+                }
+                stack.pop();
+            }
+
+            else {
+                if (stack.isEmpty() || stack.peek() != '{') {
+                    return false;
+                }
+                stack.pop();
+            }
         }
-        if (ch == '}') {
-          if (stack.isEmpty() || stack.pop() != '{') {
-            return false;
-          }
-        }
-        if (ch == ']') {
-          if (stack.isEmpty() || stack.pop() != '[') {
-            return false;
-          }
-        }
-      }
-    }
-    return stack.isEmpty();    
+
+        return stack.isEmpty();
     }
 }
