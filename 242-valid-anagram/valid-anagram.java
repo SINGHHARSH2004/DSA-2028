2882074590ede37a88681eaa837bcee5de520ace
SINGHHARSH2004 <singhh14587@gmail.com>
2026-08-09 10:19:36 +0530

    Time: 14 ms (25.97%) | Memory: 46.3 MB (48.00%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,24 +1,40 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<String,Integer> map1=new HashMap<>();
-        HashMap<String,Integer> map2= new HashMap<>();
-
+        HashMap<Character,Integer> map1=new HashMap<>();
+        HashMap<Character,Integer> map2= new HashMap<>();
+        
      
 
-    String[] arr1 = s.split("");
-    String[] arr2 = t.split("");
+    // String[] arr1 = s.split("");
+    // String[] arr2 = t.split("");
         
            
-         for(String items:arr1){
-            map1.put(items,map1.getOrDefault(items,0)+1);
-        }
+    //      for(String items:arr1){
+    //         map1.put(items,map1.getOrDefault(items,0)+1);
+    //     }
 
-        for(String items:arr2){
-            map2.put(items,map2.getOrDefault(items,0)+1);
-        }
-            if(map1.equals(map2)){
+    //     for(String items:arr2){
+    //         map2.put(items,map2.getOrDefault(items,0)+1);
+    //     }
+    //         if(map1.equals(map2)){
+    //             return true;
+    //         }
+    //         return false;
+
+    for(int i=0;i<s.length();i++){
+        char items=s.charAt(i);
+         map1.put(items,map1.getOrDefault(items,0)+1);
+    }
+
+     for(int i=0;i<t.length();i++){
+        char items=t.charAt(i);
+         map2.put(items,map2.getOrDefault(items,0)+1);
+    }
+
+             if(map1.equals(map2)){
                 return true;
             }
             return false;
+
     }
 }
