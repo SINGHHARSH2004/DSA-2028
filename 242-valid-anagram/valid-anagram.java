0feb991af7e8662036460d6791326ee198446006
SINGHHARSH2004 <singhh14587@gmail.com>
2026-08-24 18:34:50 +0530

    Time: 15 ms (23.95%) | Memory: 46.2 MB (47.64%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -21,20 +21,35 @@ class Solution {
     //         }
     //         return false;
 
+    // for(int i=0;i<s.length();i++){
+    //     char items=s.charAt(i);
+    //      map1.put(items,map1.getOrDefault(items,0)+1);
+    // }
+
+    //  for(int i=0;i<t.length();i++){
+    //     char items=t.charAt(i);
+    //      map2.put(items,map2.getOrDefault(items,0)+1);
+    // }
+
+    //          if(map1.equals(map2)){
+    //             return true;
+    //         }
+    //         return false;
+
+
     for(int i=0;i<s.length();i++){
-        char items=s.charAt(i);
-         map1.put(items,map1.getOrDefault(items,0)+1);
+        char ch=s.charAt(i);
+        map1.put(ch,map1.getOrDefault(ch,0)+1);
     }
 
-     for(int i=0;i<t.length();i++){
-        char items=t.charAt(i);
-         map2.put(items,map2.getOrDefault(items,0)+1);
+    for(int i=0;i<t.length();i++){
+        char ch=t.charAt(i);
+        map2.put(ch,map2.getOrDefault(ch,0)+1);
     }
+        if(map1.equals(map2)){
+            return true;
+        }
 
-             if(map1.equals(map2)){
-                return true;
-            }
-            return false;
-
+        return false;
     }
 }
