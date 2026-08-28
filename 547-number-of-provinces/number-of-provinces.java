f58259985ec1c847594f79edf1ee3d385c1df8f4
SINGHHARSH2004 <singhh14587@gmail.com>
2026-08-28 16:04:57 +0530

    Time: 1 ms (86.84%) | Memory: 47.5 MB (24.90%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,29 +1,31 @@
 class Solution {
     public int findCircleNum(int[][] adj) {
-        int n=adj.length;
         int count=0;
-        boolean[] vis =new boolean[n];
+        int n=adj.length;
+        boolean[] vis=new boolean[n];
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                bfs(i,vis,adj);
+                bfs(adj,i,vis);
                 count++;
             }
         }
-        return  count;
+        return count;
     }
-    private void bfs(int i,boolean[]vis,int[][] adj){
-        vis[i]=true;
+    public void bfs(int[][]adj,int i,boolean[] vis){
         int n=adj.length;
-        Queue<Integer> queue=new LinkedList<>();
+        Queue<Integer>queue=new LinkedList<>();
         queue.offer(i);
-        while(queue.size()>0){
-            int front =queue.remove();
-            for(int j=0;j<n;j++){
-                if(adj[front][j]==1 && vis[j]==false){
-                    queue.add(j);
-                    vis[j]=true;
-                }
+        vis[i]=true;
+        while(!queue.isEmpty()){
+            int front=queue.poll();
+        for(int j=0;j<n;j++){
+            
+            if(adj[front][j]==1 && vis[j]==false){
+                vis[j]=true;
+                queue.offer(j);
             }
         }
+        }
+       
     }
 }
