7aecd601b236e45b430ab7c15fa1c2c852804deb
SINGHHARSH2004 <singhh14587@gmail.com>
2026-08-29 22:27:22 +0530

    Time: 6 ms (43.81%) | Memory: 47 MB (29.77%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] findOrder(int n, int[][] pre) {
+        List<List<Integer>>adj=new ArrayList<>();
+        int[]  indegree= new int[n];
+        boolean[] vis=new boolean[n];
+            for(int i=1;i<=n;i++){
+                adj.add(new ArrayList<>());
+            }
+            for(int[] edge:pre){
+                int u=edge[0];
+                int v=edge[1];
+                adj.get(v).add(u);
+                indegree[u]++;
+            }
+
+            //khan's algo
+
+            Queue<Integer>queue=new LinkedList<>();
+            List<Integer> ans=new ArrayList<>();
+            for(int i=0;i<n;i++){
+                if(indegree[i]==0){
+                    queue.add(i);
+                    vis[i]=true;
+                }
+            }
+            while(!queue.isEmpty()){
+                int front=queue.poll();
+                ans.add(front);
+                for(int ele:adj.get(front)){
+                    indegree[ele]--;
+                    if(indegree[ele]==0){
+                        queue.offer(ele);
+                        vis[ele]=true;
+                    }
+                }
+            }
+            int[] ans2=new int[ans.size()];
+            for(int i=0;i<ans.size();i++){
+                ans2[i]=ans.get(i);
+            }
+            if(ans.size()!=n){
+                return new int[0];
+            }
+            return ans2;
+    }
+}
