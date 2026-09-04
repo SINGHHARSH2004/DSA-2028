bbba2ec2798319a73a5ecbf23a3fd41c10e899f7
SINGHHARSH2004 <singhh14587@gmail.com>
2026-09-04 18:58:09 +0530

    Time: 947 ms (6.66%) | Memory: 76.4 MB (67.58%) - LeetSync

---
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -0,0 +1,66 @@
+class Solution {
+    public class Triplet implements Comparable<Triplet> {
+        int node;
+        int parent;
+        int dist;
+
+        Triplet(int node, int parent, int dist) {
+            this.node = node;
+            this.parent = parent;
+            this.dist = dist;
+        }
+
+        public int compareTo(Triplet t) {
+            if (this.dist == t.dist)
+                return Integer.compare(this.node, t.node);
+
+            return Integer.compare(this.dist, t.dist);
+        }
+    }
+
+    public int minCostConnectPoints(int[][] points) {
+        int n = points.length;
+
+        PriorityQueue<Triplet> pq = new PriorityQueue<>();
+        pq.add(new Triplet(0, -1, 0));
+
+        int sum = 0;
+        boolean[] vis = new boolean[n];
+
+        while (pq.size() > 0) {
+
+            Triplet top = pq.remove();
+
+            int node = top.node;
+            int parent = top.parent;
+            int dist = top.dist;
+
+            if (vis[node])
+                continue;
+
+            sum += dist;
+            vis[node] = true;
+
+            for (int i = 0; i < n; i++) {
+
+                if (i == node || i == parent)
+                    continue;
+
+                if (vis[i])
+                    continue;
+
+                int x1 = points[node][0];
+                int y1 = points[node][1];
+
+                int x2 = points[i][0];
+                int y2 = points[i][1];
+
+                int mdis = Math.abs(x1 - x2) + Math.abs(y1 - y2);
+
+                pq.add(new Triplet(i, node, mdis));
+            }
+        }
+
+        return sum;
+    }
+}
