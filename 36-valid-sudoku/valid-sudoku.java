66c12b538317a6586bcc02616906d52562ba9288
SINGHHARSH2004 <singhh14587@gmail.com>
2026-09-05 18:06:04 +0530

    Time: 14 ms (5.78%) | Memory: 52.5 MB (8.34%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,70 +1,22 @@
 class Solution {
-    public boolean validSub(char[][] board, int sr, int er, int sc, int ec) {
-        HashSet<Character> st = new HashSet<>();
-        for (int row = sr; row <= er; row++) {
-            for (int col = sc; col <= ec; col++) {
-
-                char ch = board[row][col];
-
-                if (ch == '.') continue;
-
-                if (st.contains(ch))
-                    return false;
-
-                st.add(ch);
-            }
-        }
-        return true;
-    }
     public boolean isValidSudoku(char[][] board) {
-        // Validate rows
-        for (int row = 0; row < 9; row++) {
-
-            HashSet<Character> st = new HashSet<>();
-
-            for (int col = 0; col < 9; col++) {
-
-                char ch = board[row][col];
-
-                if (ch == '.') continue;
-
-                if (st.contains(ch))
+        HashSet<String>set=new HashSet<>();
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    continue;
+                }
+                String row=board[i][j] +"row"+ i;
+                String col=board[i][j] +"on"+ j;
+                String box=+board[i][j] +"on"+ (i/3) +"_"+ (j/3);
+                if(set.contains(row) || set.contains(col) || set.contains(box)){
                     return false;
-
-                st.add(ch);
+                }
+                set.add(row);
+                set.add(col);
+                set.add(box);
             }
         }
-   // Validate columns
-        for (int col = 0; col < 9; col++) {
-
-            HashSet<Character> st = new HashSet<>();
-
-            for (int row = 0; row < 9; row++) {
-
-                char ch = board[row][col];
-
-                if (ch == '.') continue;
-
-                if (st.contains(ch))
-                    return false;
-
-                st.add(ch);
-            }
-        }
-        // Validate each 3x3 box
-        for (int sr = 0; sr < 9; sr += 3) {
-
-            int er = sr + 2;
-
-            for (int sc = 0; sc < 9; sc += 3) {
-
-                int ec = sc + 2;
-
-                if (!validSub(board, sr, er, sc, ec))
-                    return false;
-            }
-        }
-
         return true;
     }
 }
